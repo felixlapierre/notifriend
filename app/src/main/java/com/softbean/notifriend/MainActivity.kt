2f8074137f88e40f1d8bcece2131dc61a8a37289
Felix <felix.lapierre1@gmail.com>
2020-03-23 14:10:01 -0400

    Added basic sample notification

--- app/src/main/java/com/softbean/notifriend/MainActivity.kt
+++ app/src/main/java/com/softbean/notifriend/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.softbean.notifriend
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
     private val channelId = "NOTIFRIEND_CHANNEL_ID"
+    private val notificationId = 176
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        //API 26+ needs a Notification Channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
     }
 
     fun onActivate(view: View) {
-        print("Activated!")
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Your lad wants to talk to you!")
+            .setContentText("He thinks you're doing great!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, builder.build())
+        }
     }
 }
