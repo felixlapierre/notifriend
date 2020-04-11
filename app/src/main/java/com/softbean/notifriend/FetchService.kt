770ebcd89900ffc91357dc314518ba2a7592908a
Felix <felix.lapierre1@gmail.com>
2020-04-11 17:56:23 -0400

    Added second half of fetch

--- app/src/main/java/com/softbean/notifriend/FetchService.kt
+++ app/src/main/java/com/softbean/notifriend/FetchService.kt
@@ -19,12 +19,23 @@ class FetchService : IntentService("FetchService") {
         for(i in 0..16) {
             val id = R.drawable::class.java.getField("fetch$i").getInt(0)
             FetchNotification(this, id).send()
-            try {
-                Thread.sleep(50)
-            } catch (ex: InterruptedException) {
-                Thread.currentThread().interrupt()
-            }
+            sleep(100)
         }
         FetchNotification(this, R.drawable.fetchblank).send()
+        sleep(2000)
+        for(i in 0..15) {
+            val id = R.drawable::class.java.getField("fetch2_$i").getInt(0)
+            FetchNotification(this, id).send()
+            sleep(100)
+        }
+        IntroNotification(this).send()
+    }
+
+    private fun sleep(millis: Long) {
+        try {
+            Thread.sleep(millis)
+        } catch (ex: InterruptedException) {
+            Thread.currentThread().interrupt()
+        }
     }
 }
