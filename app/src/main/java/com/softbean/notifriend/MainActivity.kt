6a64d18725c48621da41e0d2b57cbe114b3c5d81
Felix <felix.lapierre1@gmail.com>
2020-04-15 16:15:10 -0400

    Made Notifriend notifications silent

--- app/src/main/java/com/softbean/notifriend/MainActivity.kt
+++ app/src/main/java/com/softbean/notifriend/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val importance = NotificationManager.IMPORTANCE_LOW
             val channel = NotificationChannel(channelId, name, importance).apply {
                 description = descriptionText
             }
